b0e87676c8a5518def1ace2a680c08d38b90445c
Tomas Garcia <tomasgarciadellacqua@gmail.com>
2022-10-14 21:22:02 -0300

    Trabajo entregable

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from ejemplo.models import Familiar
 
 # Create your views here.
 def index(request):
@@ -16,3 +17,7 @@ def index_dos(request, nombre, apellido):
 def index_tres(request):
     return render(request, 'ejemplo/saludar.html',
     {'notas':[1,2,3,4,5,6,7,8]})
+
+def monstrar_familiares(request):
+    lista_familiares = Familiar.objects.all()
+    return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
